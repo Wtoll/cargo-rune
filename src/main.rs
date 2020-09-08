021b5e66cb4256eb365ab6021819949969de1765
Will Toll <will@wtoll.com>
2020-09-07 22:37:35 -0600

    Start of a basic Clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let authors = env!("CARGO_PKG_AUTHORS").replace(':', "\n");
+
+    let mut cli = App::new("Cargo Rune")
+        .bin_name("cargo rune")
+        .author(&*authors)
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(App::new("run")
+            .name("run")
+            .about("runs a rune script"))
+        .subcommand(App::new("version")
+            .name("version")
+            .about("gets the current cargo rune version"));
+
+    let command_match = cli.get_matches_mut();
+
+    match command_match.subcommand_name() {
+        Some(contents) => match contents {
+            "run" => subcommands::run(),
+            "version" => {
+                cli.write_version(&mut io::stdout()).expect("failed to write to stdout");
+                println!();
+            },
+            _ => {}
+        },
+        None => subcommands::run()
+    }
 }
+
+mod subcommands;
---
+++ src/subcommands.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    // Eventually will run a rune script at a given path
+    println!("Run command");
+}
